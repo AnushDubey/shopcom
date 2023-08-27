657d8a209334991207a864e76d0ad4dfade522be
AnushDubey <anush20.12.20@gmail.com>
2023-08-27 17:24:40 +0530

    repo link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-white text-center py-3'>&copy; Made by Anush Dubey  <br />
-          <a className='text-white text-center py-3' href='https://www.github.com' target='_blank' rel='noopener noreferrer'>
+          <a className='text-white text-center py-3' href='https://github.com/AnushDubey/shopcom' target='_blank' rel='noopener noreferrer'>
                Github
           </a>
           </Col>
